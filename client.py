e5a39916102ec0ebcec8ae85c8ccf58a33ad963f
parkseungje <tmdwprpdla@naver.com>
2021-03-12 18:07:39 +0900

    터미널 채팅앱 개발 완료

--- client.py
+++ client.py
@@ -1,8 +1,26 @@
 import socket
+import select
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 8000)) # 서버에 접속한다 127.0.0.1 == localhost == 내 컴퓨터
 
+name = None # 자신의 이름을 저장할 변수
 
+while True:
+    read, write, fail = select.select((s, sys.stdin), (), ()) # 소켓에서 메시지를 읽을수 있을때까지 대기
+
+    for desc in read:
+        if desc == s:
+            data = s.recv(4096) # 메시지가 도착하면 소켓에서 4096바이트를 읽는다.
+            print(data.decode()) # 바이트 -> 문자열 출력
+
+            if name is None:
+                name = data.decode() #처음 접속했다면 부여받은 이름을 저장하고
+                s.send(f'{name} is connected!'.encode()) # 다른사람에게 접속 사실을 알린다.
+        else:
+            msg = desc.readline()
+            msg = msg.replace('\n', '')
+            s.send(f'{name} {msg}'.encode())
 
 
--- server.py
+++ server.py
@@ -1,12 +1,38 @@
 from twisted.internet import protocol, reactor
+import names
+
+transports = set() # 클라이언트를 저장할 변수
+users = set()
+
+COLORS = [ # 터미널 컬러코드
+    '\033[31m', # RED
+    '\033[32m', # GREEN
+    '\033[33m', # YELLOW
+    '\033[34m', # BLUE
+    '\033[35m', # MAGENTA
+    '\033[36m', # CYAN
+    '\033[37m', # WHITE
+    '\033[4m',  # UNDERLINE
+]
 
 class Chat(protocol.Protocol):
     def connectionMade(self):
-        print('connected') #사용자가 서버에 접속하면 connected 메시지 출력
+        # print('connected') 첫테스트. 사용자가 서버에 접속하면 connected 메시지 출력
+        # self.transport.write('connected'.encode())  둘테스트.클라이언트가 연결되면 connected 메시지 출력 문자열 -> 바이트
+        name = names.get_first_name() #랜덤한 이름을 생성한다.
+        color = COLORS[len(users) % len(COLORS)]
+        users.add(name) # 랜덤한 이름을 users에 추가한다.
+        transports.add(self.transport) # 사용자가 접속하면 transport(클라이언트)추가
         
-    def dataReceived(self, data):
-        print(data) # 사용자가 서버에 메시지를 보내면 실행, 사용자메시지(data)출력
+        self.transport.write(f'{color}{name}\033[0m'.encode()) #사용자가 접속하면 이름을 부여한다. 이름에만 색을칠하고 메시지는 칠하지않는다.
 
+    def dataReceived(self, data):
+        # print(data)  첫테스트. 사용자가 서버에 메시지를 보내면 실행, 사용자메시지(data)출력
+
+        for t in transports:
+            if self.transport is not t:
+                t.write(data)
+                
 class ChatFactory(protocol.Factory):
     def buildProtocol(self, addr):
         return Chat() # 통신프로토콜 정의
